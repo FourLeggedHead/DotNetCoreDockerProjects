378427d4aef176d949c65918bf258dbd6c7989a2
Eric Lauret <eric.lauret@live.com>
2020-01-29 16:03:09 +0100

    Added all the files for the first .NET Core Docker App

---
+++ FirstCoreDockerApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FirstCoreDockerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            var max = args.Length != 0 ? int.Parse(args[0]) : -1;
+            while (max == -1 || count < max)
+            {
+                count++;
+                Console.WriteLine($"Counter : {count}");
+                System.Threading.Tasks.Task.Delay(1000).Wait();
+            }
+        }
+    }
+}
